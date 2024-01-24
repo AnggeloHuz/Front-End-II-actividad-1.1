558ecdcdaeafdfa3b2615131d915ec23c89a0b66
Javier1432 <jjavier1282@hotmail.com>
2024-01-24 12:30:49 -0400

    Agregar el componente Nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primario': '#2595B8',
+        'secundario': '#314A52',
+        'llamativo': '#00B4EB',
+        'tono-bajo': '#357285',
+        'opaco': '#293133',
+        'blanco': '#ffffff',
+        'negro': '#000000'
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+        Nunito: ['Nunito Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
